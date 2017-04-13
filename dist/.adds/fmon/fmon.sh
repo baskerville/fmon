a028f3ff096572ecf995dd96fae6f6b83570c24d
Bastien Dejean <nihilhill@gmail.com>
2017-04-13 12:33:22 +0200

    Skip missing icons

--- dist/.adds/fmon/fmon.sh
+++ dist/.adds/fmon/fmon.sh
@@ -8,5 +8,6 @@ SCRIPTS_DIR="${SD_DIR}/.adds"
 [ -e /mnt/onboard/.kobo/version ] || exit 1
 
 for app in koreader sketch; do
+	[ -e "${ICONS_DIR}/${app}.png" ] || continue
 	"$FMON_BIN" "${ICONS_DIR}/${app}.png" "${SCRIPTS_DIR}/${app}/${app}.sh" > "${SCRIPTS_DIR}/fmon/${app}.log" 2>&1 &
 done
