93796ab99b496bb41f74328a0c58febf2f97e937
Bastien Dejean <nihilhill@gmail.com>
2017-04-13 12:29:27 +0200

    Use one log file for each instance

--- dist/.adds/fmon/fmon.sh
+++ dist/.adds/fmon/fmon.sh
@@ -4,11 +4,9 @@ SD_DIR=/mnt/onboard
 FMON_BIN=/usr/local/fmon/bin/fmon
 ICONS_DIR="${SD_DIR}/apps"
 SCRIPTS_DIR="${SD_DIR}/.adds"
-LOGFILE="${SD_DIR}/.adds/fmon/fmon.log"
 
 [ -e /mnt/onboard/.kobo/version ] || exit 1
-[ -e "$LOGFILE" ] && rm "$LOGFILE"
 
 for app in koreader sketch; do
-	"$FMON_BIN" "${ICONS_DIR}/${app}.png" "${SCRIPTS_DIR}/${app}/${app}.sh" >> "$LOGFILE" 2>&1 &
+	"$FMON_BIN" "${ICONS_DIR}/${app}.png" "${SCRIPTS_DIR}/${app}/${app}.sh" > "${SCRIPTS_DIR}/fmon/${app}.log" 2>&1 &
 done
