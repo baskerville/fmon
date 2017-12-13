7cc81537b363fa01931e84749a87e4af386d28a1
Bastien Dejean <nihilhill@gmail.com>
2017-12-13 10:54:38 +0100

    Don't risk writing to an unmounted partition

--- fmon.sh
+++ fmon.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
-SD_DIR=/mnt/onboard
-FMON_BIN=/usr/local/fmon/bin/fmon
-ICONS_DIR="${SD_DIR}/icons"
-SCRIPTS_DIR="${SD_DIR}/.adds"
-
 [ -e /mnt/onboard/.kobo/version ] || exit 1
+
+FMON_HOME=/usr/local/fmon
+FMON_BIN="${FMON_HOME}/bin/fmon"
+SD_ROOT=/mnt/onboard
+ICONS_DIR="${SD_ROOT}/icons"
+SCRIPTS_DIR="${SD_ROOT}/.adds"
 
 for icon in "${ICONS_DIR}"/*.png; do
 	app=$(basename "$icon" .png)
-	"$FMON_BIN" "$icon" "${SCRIPTS_DIR}/${app}/${app}.sh" > "${SCRIPTS_DIR}/fmon/${app}.log" 2>&1 &
+	"$FMON_BIN" "$icon" "${SCRIPTS_DIR}/${app}/${app}.sh" > "${FMON_HOME}/${app}.log" 2>&1 &
 done
